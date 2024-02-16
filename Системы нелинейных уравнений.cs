b72884eabd8641034f15084ff9bf6aafe21649dd
Mac1eer <147583135+Mac1eer@users.noreply.github.com>
2024-02-17 00:34:12 +0300

    Add files via upload

---
+++ Системы нелинейных уравнений.cs
@@ -0,0 +1,33 @@
+﻿static double Function(double x) //Создаем метод для записи фуккции, принимающая дробное значение 
+{
+    return 3 * Math.Pow(x, 4) + 4 * Math.Pow(x, 3) - 12 * x * x + 1; //Сама функция
+}
+static double MethodBisection(double a, double b, double tol) //Создаем метод для итераций 
+{
+    if (Function(a) * Function(b) >= 0)
+    {
+        throw new Exception("Функция имеет одинаковый знак в точках a и b."); //Проверяет, совподает ли знак функции при значениях а и b, и если да, выводит исключение 
+    }
+    double c = a; //Присваиваем "с" значение "а", чтобы в последствии вернуть "с"
+    while ((b - a) >= tol) //Начало итераций
+    {
+        c = (a + b) / 2; //Вычисление среднего значения
+        if (Function(c) * Function(a) > 0)
+        {
+            a = c;
+        }
+        else
+        {
+            b = c;
+        }
+    }
+    return c;
+}
+    double a = -3;
+    double b = -2;
+    double tol = 0.0001;
+    double result = MethodBisection(a, b, tol);
+    Console.WriteLine($"Ответ: {result}");
+
+
+
